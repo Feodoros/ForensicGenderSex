00937b224cf85851e18a1d464be8f637d5facaf8
Fedor Zhilkin <fzhilkin@mail.ru>
2021-04-30 01:09:23 +0300

    Add helpers

---
+++ PredictGenderWPF/PredictGenderWPF/MouseBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace PredictGenderWPF
+{
+    public class MouseBehaviour : System.Windows.Interactivity.Behavior<FrameworkElement>
+    {
+        public static readonly DependencyProperty MouseYProperty = DependencyProperty.Register(
+            "MouseY", typeof(double), typeof(MouseBehaviour), new PropertyMetadata(default(double)));
+
+        public double MouseY
+        {
+            get { return (double)GetValue(MouseYProperty); }
+            set { SetValue(MouseYProperty, value); }
+        }
+
+        public static readonly DependencyProperty MouseXProperty = DependencyProperty.Register(
+            "MouseX", typeof(double), typeof(MouseBehaviour), new PropertyMetadata(default(double)));
+
+        public double MouseX
+        {
+            get { return (double)GetValue(MouseXProperty); }
+            set { SetValue(MouseXProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseDown += AssociatedObjectOnMouseMove;
+        }
+
+        private void AssociatedObjectOnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            var pos = mouseEventArgs.GetPosition(AssociatedObject);
+            MouseX = pos.X;
+            MouseY = pos.Y;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseDown -= AssociatedObjectOnMouseMove;
+        }
+    }
+}
---
+++ PredictGenderWPF/PredictGenderWPF/PrepairedFace.cs
@@ -0,0 +1,18 @@
+﻿namespace PredictGenderWPF
+{
+    public class PrepairedFace
+    {
+        public string Path { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int X1 { get; set; }
+        public int X2 { get; set; }
+        public int Y1 { get; set; }
+        public int Y2 { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
